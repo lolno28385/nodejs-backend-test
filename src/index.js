5fc392cbbeed94001c12b26d9fce35dfddffde79
Sina Montazeri <cnacode@sinamontazeri.com>
2019-12-10 21:16:24 +0330

    add logger and error handling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import http from 'http';
 import express from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import initializeDb from './db';
 import middleware from './middleware';
@@ -9,13 +8,28 @@ import events from './routes/events';
 import config from './config.json';
 import asyncHandler from 'express-async-handler';
 import { version } from '../package.json';
+import winston, { transports } from 'winston';
+import errors from './lib/errors';
 
+//set up logger
+const logger = winston.createLogger({
+	level: 'info',
+	format: winston.format.json(),
+	defaultMeta: { service: 'user-service' },
+	transports: [
+		new transports.Console({
+			level: 'info',
+			format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.simple()
+			)
+		})
+	]
+});
 
 let app = express();
 app.server = http.createServer(app);
 
-// logger
-app.use(morgan('dev'));
 
 // 3rd party middleware
 app.use(cors({
@@ -28,26 +42,59 @@ app.use(bodyParser.json({
 
 // connect to db
 initializeDb( db => {
-
 	const dependencies = {
 		config,
 		db,
-		asyncHandler
-	}
+		asyncHandler,
+		logger,
+		errors
+	};
 
-	// internal middleware
+	//internal middleware
 	app.use(middleware(dependencies));
 
-	// event router
+	//event router
 	app.use('/', events(dependencies));
 
 	//meta router for app info, current version etc
-	app.use('/meta', (req, res, next) => {
+	app.use('/meta', (req, res) => {
 		res.json({version})
 	})
 
+
+
+	//error handler, must be after everything else
+	app.use((error, req, res) => {
+		if(!error){
+			error = errors.generalError(
+				{
+					comment: 'no error has reached error handler',
+					data: [
+						req.body, req.query, req.params
+					]
+				}
+			)
+		}
+
+
+		//send response
+		res.status(error.status);
+		
+		if (process.env.NODE_ENV !== 'development'){
+			return res.json({
+				status: error.status,
+				message: error.message,
+				stack: error.stack
+			});
+		}
+		return res.json({
+			status: error.status,
+			message: error.message
+		});
+	});
+
 	app.server.listen(process.env.PORT || config.port, () => {
-		console.log(`Started on port ${app.server.address().port}`);
+		logger.info(`Started on port ${app.server.address().port}`)
 	});
 });
 
